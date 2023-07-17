da94e959b6718eb1473b29bcac42d03d7402c4fe
Dnllcs <daniellcs04@gmail.com>
2023-07-17 17:12:10 -0300

    Projeto criado com spring initializr

---
+++ src/main/java/org/osrapazes/portalaluno/PortalAlunoApplication.java
@@ -0,0 +1,13 @@
+package org.osrapazes.portalaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAlunoApplication.class, args);
+	}
+
+}
